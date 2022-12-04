7a934835192563c5230ec507acab238ca8c17b3a
Gabriel Junges Baratto <gabrieljbaratto@gmail.com>
2022-12-04 10:58:30 -0300

    fix(MainScreen.java): invertendo a impressão dos timers na tela (estava printando a do carro 1 na tela 2, e vice versa)

--- core/src/com/mygdx/game/screens/MainScreen.java
+++ core/src/com/mygdx/game/screens/MainScreen.java
@@ -173,7 +173,7 @@ public class MainScreen implements Screen {
         batch.setProjectionMatrix(camera.combined);
 
 
-        printTime(car1,alturaTexto);
+        printTime(car1,car2.getTotalSEC(),alturaTexto);
         bitmapFont.draw(batch,"VOLTA" + "     "+car2Lap+" de "+totalLaps, car1.getX()+70, car1.getY()+alturaTexto+15);
         //printNameTag(car2,NameTAG1);
 
@@ -196,7 +196,7 @@ public class MainScreen implements Screen {
         car1.draw(batch, delta);
         batch.setProjectionMatrix(camera.combined);
 
-        printTime(car2,alturaTexto);
+        printTime(car2,car1.getTotalSEC(),alturaTexto);
         bitmapFont.draw(batch,"VOLTA" + "     "+car1Lap+" de "+totalLaps, car2.getX()+70, car2.getY()+alturaTexto+15);
         //printNameTag(car1,NameTAG2);
 
@@ -276,8 +276,7 @@ public class MainScreen implements Screen {
     }
 
     //PRINTAR CORRETAMENTE O TEMPO NO FORMATO 00.00.00
-    public void printTime(Car car,int alturaTexto){
-        long totalSEC = car.getTotalSEC();
+    public void printTime(Car car,long totalSEC,int alturaTexto){
         long sec = totalSEC % 60;
         long min = (totalSEC % 3600) / 60;
         long hours = totalSEC / 3600;
